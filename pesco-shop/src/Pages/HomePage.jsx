93b73127fa68463ac1714bf391b289f95e9e57af
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-04-22 22:35:04 +0330

    off part complete

--- pesco-shop/src/Pages/HomePage.jsx
+++ pesco-shop/src/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Banner from "../components/Banner";
 import Category from "../components/Category";
+import Off from "../components/Off";
 
 export default function HomePage(){
     return(
         <>
         <Banner />
         <Category />
+        <Off />
         </>
     )
 }
--- pesco-shop/src/components/Off/index.jsx
+++ pesco-shop/src/components/Off/index.jsx
@@ -1,1 +1,44 @@
-export default function Off(){}
+export default function Off() {
+  return (
+    <>
+      <div className="bg-[#fffaf3]">
+        <div className="flex flex-col items-end container-primary gap-4 md:flex-row md:justify-around">
+          <div className="border-2 rounded-2xl p-3 flex flex-col justify-between gap-2 md:mt-3">
+            <div>
+              <div>
+                <p>تا</p>
+                <p className="mr-2 font-bold text-4xl">50%</p>
+              </div>
+              <div>
+                <h1 className="font-bold text-[#5a5858] text-xl">لباس تابستانی انحصاری کودکان و بزرگسالان</h1>
+              </div>
+              <div className="mt-2">
+                <button className="bg-black text-white rounded-3xl p-2">اکنون خرید کنید</button>
+              </div>
+            </div>
+            <div>
+              <img src="/images/banner/banner-1.png" alt="" className="rounded-2xl"/>
+            </div>
+          </div>
+          <div className="border-2 rounded-2xl p-3 flex flex-col justify-between gap-2 md:mt-3">
+            <div>
+              <div>
+                <p>تا</p>
+                <p className="mr-2 font-bold text-4xl">70%</p>
+              </div>
+              <div>
+                <h1 className="font-bold text-[#5a5858] text-xl">لباس تابستانی انحصاری کودکان و بزرگسالان</h1>
+              </div>
+              <div className="mt-2">
+                <button className="bg-black text-white rounded-3xl p-2">اکنون خرید کنید</button>
+              </div>
+            </div>
+            <div>
+              <img src="/images/banner/banner-2.png" alt="" className="rounded-2xl"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
