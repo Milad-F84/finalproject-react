59aac1801090485cddb2e4cc6bfe8e1aa681bfe7
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-04-24 22:10:03 +0330

    all products css added

--- pesco-shop/src/Pages/HomePage.jsx
+++ pesco-shop/src/Pages/HomePage.jsx
@@ -4,33 +4,44 @@ import Off from "../components/Off";
 import SingleProduct from "../components/SingleCard";
 import useGetProducts from "../request/useGetProducts";
 
-export default function HomePage(){
-    debugger;
-    const { data: products, isLoading, error, isError } = useGetProducts();
-    if (isLoading) {
-        return <h1>LOADING...</h1>;
-      }
-    
-      if (isError) {
-        return <h1>an error has been accured{error.message}</h1>;
-      }
+export default function HomePage() {
+  const { data: products, isLoading, error, isError } = useGetProducts();
+  if (isLoading) {
+    return <h1>LOADING...</h1>;
+  }
 
-    return(
-        <>
-        <Banner />
-        <Category />
-        <Off />
-        
-        <div className="grid grid-cols-4 gap-8">
-        {products?.data?.map((item) => (
-          <SingleProduct
-            key={item.id}
-            id={item.id}
-            image={item.image}
-            title={item.title}
-          />
-        ))}
+  if (isError) {
+    return <h1>an error has been accured{error.message}</h1>;
+  }
+
+  return (
+    <>
+      <Banner />
+      <Category />
+      <Off />
+
+      <div className="bg-[#fffaf3] p-10">
+        <div className="container-primary">
+        <div>
+          <p className="text-red-500">محصولات ویژه</p>
+        </div>
+        <div>
+          <h1 className="font-bold text-2xl">مجموعه ویژگی های ما</h1>
+        </div>
+        </div>
+        <div className="grid md:grid-cols-4 gap-8 container-primary ">
+          {products?.data?.map((item) => (
+            <SingleProduct
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              rate={item.rating.rate}
+              price={item.price}
+            />
+          ))}
+        </div>
       </div>
-        </>
-    )
+    </>
+  );
 }
--- pesco-shop/src/components/SingleCard/index.jsx
+++ pesco-shop/src/components/SingleCard/index.jsx
@@ -1,20 +1,34 @@
 import { useNavigate } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
 
-export default function SingleProduct({ image, title, id }) {
-    const navigate = useNavigate();
+export default function SingleProduct({ image, title, id, rate, price }) {
+  const navigate = useNavigate();
 
-    return (
-      <div
-        onClick={() => navigate(`/products/${id}`)}
-        className="shadow-lg rounded-md overflow-hidden cursor-pointer"
-      >
-        <img
-          className="w-full aspect-square object-contain"
-          src={image}
-          alt="product image"
-        />
-  
-        <h2>{title}</h2>
+  return (
+    <div
+      onClick={() => navigate(`/products/${id}`)}
+      className="shadow-lg rounded-md overflow-hidden cursor-pointer p-3"
+    >
+      <img
+        className="w-full aspect-square object-contain rounded-3xl"
+        src={image}
+        alt="product image"
+      />
+      <div className="flex items-center gap-1">
+        <div className="flex items-center gap-1">
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+        </div>
+        <p>({rate})</p>
       </div>
-    );
+      <div className="flex justify-between items-center gap-2">
+        <h2 className="font-bold hover:text-red-500 transition line-clamp-1">
+          {title}
+        </h2>
+        <p>{price}$</p>
+      </div>
+    </div>
+  );
 }
