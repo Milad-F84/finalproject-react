be1ed54121988aacefafcd1c2a2f75fc1ed8702a
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-04-24 21:11:46 +0330

    all products added

--- pesco-shop/src/Pages/HomePage.jsx
+++ pesco-shop/src/Pages/HomePage.jsx
@@ -1,13 +1,36 @@
 import Banner from "../components/Banner";
 import Category from "../components/Category";
 import Off from "../components/Off";
+import SingleProduct from "../components/SingleCard";
+import useGetProducts from "../request/useGetProducts";
 
 export default function HomePage(){
+    debugger;
+    const { data: products, isLoading, error, isError } = useGetProducts();
+    if (isLoading) {
+        return <h1>LOADING...</h1>;
+      }
+    
+      if (isError) {
+        return <h1>an error has been accured{error.message}</h1>;
+      }
+
     return(
         <>
         <Banner />
         <Category />
         <Off />
+        
+        <div className="grid grid-cols-4 gap-8">
+        {products?.data?.map((item) => (
+          <SingleProduct
+            key={item.id}
+            id={item.id}
+            image={item.image}
+            title={item.title}
+          />
+        ))}
+      </div>
         </>
     )
 }
--- pesco-shop/src/components/SingleCard/index.jsx
+++ pesco-shop/src/components/SingleCard/index.jsx
@@ -1,1 +1,20 @@
-export default function SingleCard(){}
+import { useNavigate } from "react-router-dom";
+
+export default function SingleProduct({ image, title, id }) {
+    const navigate = useNavigate();
+
+    return (
+      <div
+        onClick={() => navigate(`/products/${id}`)}
+        className="shadow-lg rounded-md overflow-hidden cursor-pointer"
+      >
+        <img
+          className="w-full aspect-square object-contain"
+          src={image}
+          alt="product image"
+        />
+  
+        <h2>{title}</h2>
+      </div>
+    );
+}
