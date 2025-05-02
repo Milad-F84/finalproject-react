e4116b5258e95c269ca260b8e496e6d099729fd3
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-05-02 20:08:50 +0330

    single product page complete

--- pesco-shop/src/Pages/SingleProductPage.jsx
+++ pesco-shop/src/Pages/SingleProductPage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useGetSingleProduct from "../request/useGetSingleProduct";
 import StarIcon from "@mui/icons-material/Star";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 
 export default function SingleProductPage() {
   const { productId } = useParams();
@@ -25,27 +26,45 @@ export default function SingleProductPage() {
   }
 
   return (
-    <div className="shadow-lg rounded-2xl overflow-hidden cursor-pointer p-3">
-      <img
-        className="w-full aspect-square object-contain rounded-3xl"
-        src={data?.data?.image}
-        alt="product image"
-      />
-      <div className="flex items-center gap-1">
-        <div className="flex items-center gap-1">
-          <StarIcon className="text-red-500" />
-          <StarIcon className="text-red-500" />
-          <StarIcon className="text-red-500" />
-          <StarIcon className="text-red-500" />
+    <>
+      <div className="flex flex-col gap-7 md:flex-row md:justify-between md:items-center container-primary p-4">
+        <img
+          src={data?.data?.image}
+          alt="product image"
+          className="w-full aspect-square object-contain rounded-3xl md:w-[600px]"
+        />
+        <div className="flex flex-col gap-3">
+          <div className="flex justify-start items-center gap-1">
+            <LocalOfferIcon className="text-red-700" />
+            <p className="text-red-700">فروش 70% تخفیف</p>
+          </div>
+          <h1 className="text-2xl font-bold">{data?.data?.title}</h1>
+          <div className="flex items-center gap-1">
+            <div className="flex items-center gap-1">
+              <StarIcon className="text-red-500" />
+              <StarIcon className="text-red-500" />
+              <StarIcon className="text-red-500" />
+              <StarIcon className="text-red-500" />
+            </div>
+            <p>({data?.data?.rating?.rate})</p>
+          </div>
+          <h2>{data?.data?.description}</h2>
+          <hr className="text-gray-400"/>
+          <p className="font-bold text-lg">{data?.data?.price}$</p>
+          <div className="md:flex md:items-center md:gap-5">
+            <div className="border-2 border-gray-400 rounded-3xl p-3 max-w-[150px] flex gap-2 justify-between items-center h-[40px]">
+              <button className="text-2xl">+</button>
+              <span className="text-xl">2</span>
+              <button className="text-2xl">-</button>
+            </div>
+            <div>
+              <button className="bg-black text-white max-w-[150px] h-[40px] rounded-3xl p-2 mb-3 text-center">
+                افزودن به سبد خرید
+              </button>
+            </div>
+          </div>
         </div>
-        <p>({data?.data?.rating?.rate})</p>
       </div>
-      <div className="flex justify-between items-center gap-2">
-        <h2 className="font-bold hover:text-red-500 transition line-clamp-1">
-          {data?.data?.title}
-        </h2>
-        <p>{data?.data?.price}$</p>
-      </div>
-    </div>
+    </>
   );
 }
