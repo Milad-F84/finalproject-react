b3787066909823a28e07b462f852cbdaa72429e3
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-05-08 22:26:21 +0330

    breadcrumb for single product is added

--- pesco-shop/src/Pages/SingleProductPage.jsx
+++ pesco-shop/src/Pages/SingleProductPage.jsx
@@ -4,11 +4,15 @@ import StarIcon from "@mui/icons-material/Star";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import useCart from "../store/useCart";
 import { useMemo } from "react";
+import { Breadcrumbs, Typography, Link } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import HomeIcon from "@mui/icons-material/Home";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 
 export default function SingleProductPage() {
   const { productId } = useParams();
   const { data, isLoading, error, isError } = useGetSingleProduct(productId);
-  const { products, addProduct, removeProduct} = useCart();
+  const { products, addProduct, removeProduct } = useCart();
   console.log(data);
 
   const thisProductCount = useMemo(() => {
@@ -40,6 +44,25 @@ export default function SingleProductPage() {
 
   return (
     <>
+      <div className="max-w-6xl mx-auto px-4 pt-4">
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          <Link
+            underline="hover"
+            color="inherit"
+            href="/"
+            sx={{ display: "flex", alignItems: "center", gap: 0.5 }}
+          >
+            <HomeIcon fontSize="small" />
+            خانه
+          </Link>
+          <Typography color="primary">
+            {data?.data?.title?.slice(0, 25) + "..."}
+          </Typography>
+        </Breadcrumbs>
+      </div>
       <div className="flex flex-col gap-7 md:flex-row md:justify-between md:items-center container-primary p-4">
         <img
           src={data?.data?.image}
@@ -68,9 +91,19 @@ export default function SingleProductPage() {
           </p>
           <div className="flex flex-col gap-2 items-center md:flex-row md:items-center md:gap-5 ">
             <div className="border-2 border-gray-400 rounded-3xl p-3 w-[150px] flex gap-2 justify-between items-center h-[40px]">
-              <button onClick={() => addProduct(productId)} className="text-2xl cursor-pointer">+</button>
+              <button
+                onClick={() => addProduct(productId)}
+                className="text-2xl cursor-pointer"
+              >
+                +
+              </button>
               <span className="text-xl">{thisProductCount}</span>
-              <button onClick={() => removeProduct(productId)} className="text-2xl cursor-pointer">-</button>
+              <button
+                onClick={() => removeProduct(productId)}
+                className="text-2xl cursor-pointer"
+              >
+                -
+              </button>
             </div>
             <div>
               <button className="bg-black text-white w-[150px] h-[40px] rounded-3xl p-2 text-center cursor-pointer hover:bg-red-500 duration-250">
