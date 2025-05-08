be54e149dc04066de2b5e007c707d915805b2a6c
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-05-08 18:55:55 +0330

    calculate totalPrice function is added

--- pesco-shop/src/Pages/ShoppingCartPage.jsx
+++ pesco-shop/src/Pages/ShoppingCartPage.jsx
@@ -1,10 +1,22 @@
+import { useCallback, useMemo, useState } from "react";
 import ShoppingCart from "../components/ShoppingCart";
 import useCart from "../store/useCart";
+import useGetSingleProduct from "../request/useGetSingleProduct";
 
 export default function ShoppingCartPage() {
   const { products } = useCart();
 
   const totalItems = products.reduce((sum, item) => sum + item.quantity, 0);
+
+  const [priceMap, setPriceMap] = useState({});
+
+  const handlePriceCalculated = useCallback((productId, totalPrice) => {
+    setPriceMap((prev) => ({ ...prev, [productId]: totalPrice }));
+  }, []);
+
+  const totalPrice = Object.values(priceMap).reduce((sum, price) => sum + price, 0);
+
+  console.log("priceMap:", priceMap);
 
   return (
     <>
@@ -13,7 +25,8 @@ export default function ShoppingCartPage() {
           سبد خرید شما
         </h1>
         {products.length === 0 ? (
-          <div className="text-center mt-10 p-10 bg-yellow-50 rounded-xl shadow-sm">
+          <div className="text-center p-10 flex flex-col justify-center items-center">
+            <img src="\images\cart-empty.png" alt="" />
             <p className="text-lg text-gray-600">سبد خرید شما خالی است</p>
           </div>
         ) : (
@@ -23,14 +36,17 @@ export default function ShoppingCartPage() {
                 key={item.id}
                 productId={item.id}
                 quantity={item.quantity}
+                onPriceCalculated={handlePriceCalculated}
               />
             ))}
 
-            <div className="border-t pt-4 text-right">
+            <div className="border-t pt-4 text-right flex flex-col md:flex-row gap-3 md:gap-0 justify-between items-center">
               <span className="text-xl font-semibold text-gray-700">
                 مجموع تعداد کالا:{" "}
                 <span className="text-orange-500">{totalItems}</span>
               </span>
+              <p className="text-xl font-semibold text-gray-700">قیمت کل: <span className="text-orange-500">{totalPrice.toFixed(2)}$</span></p>
+              <button className="text-sm text-white bg-red-500 hover:bg-black px-4 py-1 rounded-md transition cursor-pointer duration-200">رفتن برای تسویه حساب</button>
             </div>
           </div>
         )}
--- pesco-shop/src/components/ShoppingCart/index.jsx
+++ pesco-shop/src/components/ShoppingCart/index.jsx
@@ -1,39 +1,66 @@
+import { useEffect } from "react";
 import useGetSingleProduct from "../../request/useGetSingleProduct";
 import useCart from "../../store/useCart";
 
-export default function ShoppingCart({productId , quantity}){
-    const {data , isLoading , error , isError} = useGetSingleProduct(productId);
-    const {removeProductCompletely} = useCart();
-    
-    if(isLoading){
-        return (
-            <div className="h-screen flex justify-center items-center">
-              <img src="\images\loading-shop.gif" alt="" className="" />
-            </div>
-          );
+export default function ShoppingCart({
+  productId,
+  quantity,
+  onPriceCalculated,
+}) {
+  const { data, isLoading, error, isError } = useGetSingleProduct(productId);
+  const { removeProductCompletely } = useCart();
+
+  useEffect(() => {
+    if (data?.data?.price && onPriceCalculated) {
+      const total = quantity * data.data.price;
+      onPriceCalculated(productId, total);
     }
+  }, [data, quantity]);
 
-    if (isError) {
-        return (
-          <div className="h-screen flex flex-col justify-center items-center">
-            <img src="\images\error-shop.gif" alt="" className="" />
-            <h1 className="font-bold animate-pulse text-center">an error has been accured{error.message}</h1>
-          </div>
-        );
-      }
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <img src="\images\loading-shop.gif" alt="" className="" />
+      </div>
+    );
+  }
 
-      return(
-        <>
-        <div className="flex flex-col sm:flex-row items-center gap-4 border p-4 rounded-xl shadow-md bg-white hover:shadow-lg transition">
-            <img className="w-24 h-24 object-contain" src={data?.data?.image} alt="" />
-            <div className="flex-1 w-full text-right">
-                <h3 className="text-lg font-bold text-gray-800">{data?.data?.title}</h3>
-                <p className="text-gray-600">قیمت واحد:{data?.data?.price}</p>
-                <p className="text-gray-600">{quantity}</p>
-                <p className="text-green-600 font-semibold mt-2">مجموع:{(quantity * data?.data?.price).toFixed(2)}$</p>
-                <button  className="mt-4 text-sm text-white bg-red-500 hover:bg-red-600 px-4 py-1 rounded-md transition cursor-pointer" onClick={() => removeProductCompletely(productId)}>حذف محصول</button>
-            </div>
+  if (isError) {
+    return (
+      <div className="h-screen flex flex-col justify-center items-center">
+        <img src="\images\error-shop.gif" alt="" className="" />
+        <h1 className="font-bold animate-pulse text-center">
+          an error has been accured{error.message}
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex flex-col sm:flex-row items-center gap-4 border p-4 rounded-xl shadow-md bg-white hover:shadow-lg transition">
+        <img
+          className="w-24 h-24 object-contain"
+          src={data?.data?.image}
+          alt=""
+        />
+        <div className="flex-1 w-full text-right">
+          <h3 className="text-lg font-bold text-gray-800">
+            {data?.data?.title}
+          </h3>
+          <p className="text-gray-600">قیمت واحد:{data?.data?.price}</p>
+          <p className="text-gray-600">تعداد:{quantity}</p>
+          <p className="text-green-600 font-semibold mt-2">
+            مجموع:{(quantity * data?.data?.price).toFixed(2)}$
+          </p>
+          <button
+            className="mt-4 text-sm text-white bg-red-500 hover:bg-red-600 px-4 py-1 rounded-md transition cursor-pointer"
+            onClick={() => removeProductCompletely(productId)}
+          >
+            حذف محصول
+          </button>
         </div>
-        </>
-      )
+      </div>
+    </>
+  );
 }
