1f0475ea929b2e8a30ae85bc9d36164658b85cc1
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-05-08 14:32:41 +0330

    remove productCart completely is added

--- pesco-shop/src/store/useCart.js
+++ pesco-shop/src/store/useCart.js
@@ -46,48 +46,58 @@ const useCart = create((set) => {
           };
         }
       }),
-      removeProduct: (id) =>
-        set((prev) => {
-            const foundItemIndex = prev.products.findIndex(
-              (item) => item.id === id
-            );
-            if (foundItemIndex != -1) {
-              const newProducts = prev.products.map((item, index) => {
-                if (index == foundItemIndex) {
-                  return {
-                    ...item,
-                    quantity: item.quantity - 1,
-                  };
-                } else {
-                  return item;
-                }
-              }).filter((item) => item.quantity > 0);
-              localStorage.setItem("cart", JSON.stringify(newProducts));
-              return {
-                products: newProducts,
-              };
-            } else {
-              localStorage.setItem(
-                "cart",
-                JSON.stringify([
-                  ...prev.products,
-                  {
-                    id,
-                    quantity: 0,
-                  },
-                ])
-              );
-              return {
-                products: [
-                  ...prev.products,
-                  {
-                    id,
-                    quantity: 0,
-                  },
-                ],
-              };
-            }
-          }),
+    removeProduct: (id) =>
+      set((prev) => {
+        const foundItemIndex = prev.products.findIndex(
+          (item) => item.id === id
+        );
+        if (foundItemIndex != -1) {
+          const newProducts = prev.products
+            .map((item, index) => {
+              if (index == foundItemIndex) {
+                return {
+                  ...item,
+                  quantity: item.quantity - 1,
+                };
+              } else {
+                return item;
+              }
+            })
+            .filter((item) => item.quantity > 0);
+          localStorage.setItem("cart", JSON.stringify(newProducts));
+          return {
+            products: newProducts,
+          };
+        } else {
+          localStorage.setItem(
+            "cart",
+            JSON.stringify([
+              ...prev.products,
+              {
+                id,
+                quantity: 0,
+              },
+            ])
+          );
+          return {
+            products: [
+              ...prev.products,
+              {
+                id,
+                quantity: 0,
+              },
+            ],
+          };
+        }
+      }),
+    removeProductCompletely: (id) =>
+      set((prev) => {
+        const newProducts = prev.products.filter((item) => item.id !== id);
+        localStorage.setItem("cart", JSON.stringify(newProducts));
+        return {
+          products: newProducts,
+        };
+      }),
   };
 });
 
