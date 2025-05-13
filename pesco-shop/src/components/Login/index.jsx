105b3ac9fb5bab0bdde8918b263531f784f0787a
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-05-13 17:44:16 +0330

    yup and react hook form is added

--- pesco-shop/src/components/Login/index.jsx
+++ pesco-shop/src/components/Login/index.jsx
@@ -1,1 +1,8 @@
-export default function Login(){}
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+export default function Login() {
+  const schema = yup.object({});
+}
